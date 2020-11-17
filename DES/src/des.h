9b0668742cc0e2bab770aaa9b550ce7ba238fa8f
2950833136 <2950833136@qq.com>
2020-11-17 14:03:03 +0800

    update

--- DES/src/des.h
+++ DES/src/des.h
@@ -5,20 +5,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void BitsCopy(bool *DatOut, bool *DataIn, int Len);                        // 数组复制
-void ByteToBit(bool *DatOut, char *DataIn, int Num);                       // 字节到位
-void BitToByte(char *DatOut, bool *DataIn, int Num);                       // 位到字节
-void BitToHex(char *DatOut, bool *DataIn, int Num);                        // 二进制到十六进制
-void HexToBit(bool *DatOut, char *DataIn, int Num);                        // 十六进制到二进制
-void TablePermute(bool *DatOut, bool *DataIn, const char *Table, int Num); // 位表置换函数
-void F_Change(bool DataIn[32], bool DatKi[48]);                            // F函数
-void S_Change(bool DatOut[32], bool DataIn[48]);                           // S盒置换
-void E_change(bool DataOut[48], bool DataIn[32]);                          // E置换
-void P_change(bool DataOut[32], bool DataIn[32]);                          // P置换
-void Xor(bool *DatA, bool *DatB, int Num);                                 // 异或函数
-void LoopMove(bool *DataIn, int Len, int Num);                             // 循环左移 Len长度 Num移动位数
-void SetKey(char KeyIn[8]);                                                // 设置密钥
-void Des(char MesOut[8], char MesIn[8]);                                   // 执行DES加密
-void deDes(char MesOut[8], char MesIn[8]);                                 // 执行DES解密
+void BitsCopy(bool *DatOut, bool *DataIn, int Len);                                 // 数组复制
+void ByteToBit(bool *DatOut, unsigned char *DataIn, int Num);                       // 字节到位
+void BitToByte(unsigned char *DatOut, bool *DataIn, int Num);                       // 位到字节
+void BitToHex(unsigned char *DatOut, bool *DataIn, int Num);                        // 二进制到十六进制
+void HexToBit(bool *DatOut, unsigned char *DataIn, int Num);                        // 十六进制到二进制
+void TablePermute(bool *DatOut, bool *DataIn, const unsigned char *Table, int Num); // 位表置换函数
+void F_Change(bool DataIn[32], bool DatKi[48]);                                     // F函数
+void S_Change(bool DatOut[32], bool DataIn[48]);                                    // S盒置换
+void E_change(bool DataOut[48], bool DataIn[32]);                                   // E置换
+void P_change(bool DataOut[32], bool DataIn[32]);                                   // P置换
+void Xor(bool *DatA, bool *DatB, int Num);                                          // 异或函数
+void LoopMove(bool *DataIn, int Len, int Num);                                      // 循环左移 Len长度 Num移动位数
+void SetKey(unsigned char KeyIn[8]);                                                // 设置密钥
+void Des(unsigned char MesOut[8], unsigned char MesIn[8]);                          // 执行DES加密
+void deDes(unsigned char MesOut[8], unsigned char MesIn[8]);                        // 执行DES解密
 
 #endif
--- DES/test/test.c
+++ DES/test/test.c
@@ -1,75 +1,70 @@
-/*-------------------------------------------------------
- Data Encryption Standard  56位密钥加密64位数据
- --------------------------------------------------------*/
+#include <string.h>
 
 #include "../src/des.h"
 #include "../src/table.h"
 
+void padding(unsigned char *str, int num) {
+    const unsigned char *pad = "00000000";
+    strncat(str, pad, 8 - num);
+}
+
+void displayChar(unsigned char *str, int num) {
+    for (int i = 0; i < num; i++) {
+        printf("%c", str[i]);
+    }
+    printf("\n");
+}
+
+void displayBool(bool *str, int num) {
+    for (int i = 0; i < num; i++) {
+        printf("%d", str[i]);
+    }
+    printf("\n");
+}
+
 int main() {
-    int  i            = 0;
-    int  j            = 0;
-    char MesHex[16]   = {0}; // 16个字符数组用于存放 64位16进制的密文
-    char MyKey[8]     = {0}; // 初始密钥 8字节*8
-    char YourKey[8]   = {0}; // 输入的解密密钥 8字节*8
-    char MyMessage[8] = {0}; // 初始明文
-    bool MesBit[64]   = {0}; // 明文二进制存储空间 64位
+    unsigned char  PlainText[8]  = {0}; // 明文 8 byte. 8*8=64
+    unsigned char  Key[8]        = {0}; // 密钥 8 byte,
+    unsigned char  CipherText[8] = {0}; // 密文 8 byte,
+    unsigned char  Hex[16]       = {0}; // 16 进制 64 位，4*16 =64
+    bool           Bit[64]       = {0}; // 2  进制 64 位
+    unsigned char *msg           = (unsigned char *)calloc(64, sizeof(unsigned char));
 
-    printf("Please input your Message(<=64 bit):\n");
-    gets(MyMessage); // 明文
-    printf("\n");
-    // 计算明文长度
-    while (MyMessage[j] != '\0') {
-        j++;
-    }
-
-    // 大于8提示错误
-    while (j > 8) {
-        printf("Please input a correct  Message!\n");
-        gets(MyMessage);
-        printf("\n");
-        j = 0;
-        // 再次检测
-        while (MyMessage[j] != '\0') {
-            j++;
+    printf("Please Input Message:\n");
+    scanf("%[^\n]", msg);
+    int len = strlen(msg);
+    while (len > 0) {
+        if (len > 8) {
+            strncpy(PlainText, msg, 8);
+            msg = msg + 8;
+            len = len - 8;
+        } else {
+            padding(msg, len);
+            strncpy(PlainText, msg, 8);
+            break;
         }
     }
 
-    printf("Please input your Secret Key（64bit）:\n");
-    gets(MyKey); // 密钥
-    printf("\n");
-
-    // 计算密钥长度
-    while (MyKey[i] != '\0') {
-        i++;
+    memset(msg, '\0', sizeof(unsigned char) * 64);
+    printf("Please Input Key(8 byte):\n");
+    scanf("\n%[^\n]", msg);
+    while (strlen(msg) != 8) {
+        printf("key is not 8 byte, please again...\n");
+        scanf("\n%[^\n]", msg);
     }
-    // 不是8 提示错误
-    while (i != 8) {
-        printf("Please input a correct Secret Key!\n");
-        gets(MyKey);
-        printf("\n");
-        i = 0;
-        // 再次检测
-        while (MyKey[i] != '\0') {
-            i++;
-        }
-    }
+    strncpy(Key, msg, 8);
 
-    SetKey(MyKey);          // 设置密钥 得到子密钥Ki
-    Des(MesHex, MyMessage); // 执行DES加密
+    SetKey(Key);
+    displayChar(PlainText, 8);
+    Des(CipherText, PlainText);
+    displayChar(CipherText, 8);
 
-    printf("Your Message is Encrypted!:\n"); // 信息已加密
-    for (i = 0; i < 16; i++) {
-        printf("%c ", MesHex[i]);
-    }
-    printf("\n\n");
+    ByteToBit(Bit, CipherText, 64);
+    displayBool(Bit, 64);
 
-    printf("Please input your Secret Key to Deciphering:\n"); // 请输入密钥以解密
-    gets(YourKey);                                            // 得到密钥
-    SetKey(YourKey);                                          // 设置密钥
-    deDes(MyMessage, MesHex);                                 // 解密输出到MyMessage
+    BitToHex(Hex, Bit, 64);
+    displayChar(Hex, 16);
 
-    printf("Deciphering Over !!:\n"); // 解密结束
-    for (i = 0; i < 8; i++) {
-        printf("%c ", MyMessage[i]);
-    }
+    deDes(PlainText, CipherText);
+    displayChar(PlainText, 8);
 }
