287f69f010cccbe8f1187101465420da6a96cf12
li_junfeng <li_junfeng@sicca.com.cn>
2025-11-12 17:38:50 +0800

    fix: 修复 vcpkg 加载错误

--- cmake/module_vcpkg.cmake
+++ cmake/module_vcpkg.cmake
@@ -1,38 +1,28 @@
 # 设置 vcpkg 配置
-if(CMAKE_SYSTEM_NAME MATCHES "Windows")
-    # 获取 VCPKG_ROOT
-    if(NOT DEFINED VCPKG_ROOT)
-        if(DEFINED ENV{VCPKG_ROOT})
-            set(VCPKG_ROOT "$ENV{VCPKG_ROOT}")
-        else()
-            set(VCPKG_ROOT "C:/dev/vcpkg")
-        endif()
-    endif()
-
-    # 设置工具链文件
-    set(CMAKE_TOOLCHAIN_FILE "${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
-
-    # 架构判断
-    if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-        set(VCPKG_TARGET_TRIPLET "x64-windows-static")
+if(NOT DEFINED VCPKG_ROOT)
+    if(DEFINED ENV{VCPKG_ROOT})
+        set(VCPKG_ROOT "$ENV{VCPKG_ROOT}")
     else()
-        set(VCPKG_TARGET_TRIPLET "x86-windows-static")
+        set(VCPKG_ROOT "C:/dev/vcpkg")
     endif()
-
-    # 路径输出
-    message(STATUS "VCPKG_ROOT: ${VCPKG_ROOT}")
-    message(STATUS "CMAKE_TOOLCHAIN_FILE: ${CMAKE_TOOLCHAIN_FILE}")
-    message(STATUS "VCPKG_TARGET_TRIPLET: ${VCPKG_TARGET_TRIPLET}")
-
-    # 加载工具链
-    include(${CMAKE_TOOLCHAIN_FILE})
 endif()
+
+# 设置工具链文件
+set(CMAKE_TOOLCHAIN_FILE "${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
+set(VCPKG_TARGET_TRIPLET "x64-windows-static")
+set(PKG_CONFIG_EXECUTABLE "${VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}/tools/pkgconf/pkgconf.exe")
+set(PKG_CONFIG_PATH "${VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}/lib/pkgconfig")
+
+# 路径输出
+message(STATUS "VCPKG_ROOT: ${VCPKG_ROOT}")
 
 ################################################################################
 # gmp
 #   vcpkg install gmp:x64-windows-static
 ################################################################################
-if(BUILD_VCPKG)
-    find_package(PkgConfig REQUIRED)
-    pkg_check_modules(gmp REQUIRED IMPORTED_TARGET gmp)
-endif()
+function(VCPKG_LOAD_3RDPARTY)
+    if(BUILD_VCPKG)
+        find_package(PkgConfig REQUIRED)
+        pkg_check_modules(gmp REQUIRED IMPORTED_TARGET gmp)
+    endif()
+endfunction()
